59172444ded4c9fb43f11325f3bfea6bd7cf0efd
x1aoba <xiaobayuwei@gmail.com>
2013-05-16 00:41:20 +0800

    read meta

--- app/reader/index.js
+++ app/reader/index.js
@@ -1,36 +1,67 @@
 var fs = require('fs'),
     path = require('path'),
     walk = require('./utils').walk;
+    log = require('../utils').log;
 
-
-var SUPPORT_FORMAT = ['mkd', 'md', 'markdown'];
+var logger = new log('READER');
 
 function read(callback){
   var _this = this;
 
   var content_abspath = path.join(this.root_path, this.content_path),
       content_index = path.resolve(content_abspath).split(path.sep).pop();
+
   walk(content_abspath, function(tree){
-    pullTree.call(_this, tree[content_index], callback);
+    pullTree.call(_this, tree[content_index]);
+    _this.raw_content.forEach(function(post_path, index){
+      buildPost.call(_this, post_path, index);
+    });
+    callback();
   });
 }
 
 
 /*===Helpers===*/
 
-function pullTree(tree, callback){
-  console.log(tree);
+function pullTree(tree){
   for (var node in tree){
     if (typeof tree[node] == 'object') { // folder found
         this.folders.push(node);
-        arguments.callee.call(this, tree[node], callback);
+        arguments.callee.call(this, tree[node]);
     }
     else {
       this.raw_content.push(tree[node]);
     }
   }
+}
 
-  callback()
+function buildPost(post_path, index){
+  var _this = this;
+  var post = {};
+  var data = fs.readFileSync(post_path, {encoding: 'utf8'});
+    
+  var meta, content;
+  var all = data.split('======');
+  meta = all[1];
+  content = all[2];
+
+  if (meta == undefined || content == undefined) {
+    logger.warning('Bad Post :' + post_path);
+    return;
+  }
+
+  var metas = meta.split('\n');
+  for (var key in metas){
+    var name = metas[key].split(':')[0],
+        value = metas[key].split(':')[1];
+    if (name){
+      post[name] = value;
+    }else continue;
+  }
+  post.abs_path = post_path;
+  post.parent_folder = path.dirname(post_path).split(path.sep).pop();
+
+  _this.raw_content[index] = post;
 }
 
 exports.read = read;
