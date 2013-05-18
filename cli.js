aae1cdacacbabad6f4b76950b07ee2dadebcefc6
x1aoba <xiaobayuwei@gmail.com>
2013-05-18 17:51:07 +0800

    runtime log

--- cli.js
+++ cli.js
@@ -2,9 +2,11 @@
 
 var commander = require('commander'),
     Application = require('./index').App,
+    logger = require('./app/utils').log;
     config = require('./config');
 
-var app = new Application(config);
+var app = new Application(config),
+    log = new logger('Marlboro');
 
 commander.version(require('./package').version)
 .option('-c, --create', 'Create new blog environment')
@@ -15,9 +17,13 @@ if (commander.create) {
   app.build();
 }
 else {
+  log.info('Startin');
   app.init(function(){
+    log.info('Reading');
     app.read(function(){
+      log.info('Writing');
       app.write();
+      log.info('Finished');
     });
   });
 }
