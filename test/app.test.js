6692b1decfb6a62cecba2657f62b3498bf149878
x1aoba <xiaobayuwei@gmail.com>
2013-06-08 09:59:14 +0800

    test

--- test/app.test.js
+++ test/app.test.js
@@ -10,5 +10,5 @@ var Application = new Marlboro(config);
 Application.read();
 Application.should.have.property('raw_content');
 Application.raw_content.forEach(function(post, index){
-  post.should.have.keys('title', 'date', 'tags', 'content', 'abs_path', 'target_folder', 'folder', 'basename', 'perm_link');
+  post.should.have.keys('_title', 'title', 'date', 'tags', 'content', 'abs_path', 'target_folder', 'folder', 'basename', 'perm_link');
 });
