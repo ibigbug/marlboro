b2476198a9f654327528df34c12757b535e9c160
x1aoba <xiaobayuwei@gmail.com>
2013-06-08 09:17:05 +0800

    add server.js

---
+++ app/utils/server.js
@@ -0,0 +1,53 @@
+#!/usr/bin/env node
+
+var http = require('http'),
+    url = require('url'),
+    path = require('path'),
+    fs = require('fs');
+
+function SimpleHTTPServer(dir, port){
+  this.dir = dir || process.cwd();
+  this.port = port || 8888;
+}
+
+SimpleHTTPServer.prototype.run = function(){
+  var server = this;
+  console.log('Server running at\n  => http://localhost:' + this.port);
+  http.createServer(function(req, res){
+    var uri = url.parse(req.url).pathname,
+        filename = path.join(server.dir, uri);
+
+
+    fs.exists(filename, function(exists){
+      if(!exists){
+        res.writeHead(404, {'Content-Type': 'text/plain'});
+        res.write('404 not found\n');
+        res.end();
+        return;
+      }
+
+      if (fs.statSync(filename).isDirectory()) filename += 'index.html';
+
+      fs.readFile(filename, 'binary', function(err, file){
+        if (err){
+          res.writeHead(500, {'Content-Type': 'text/plain'});
+          res.write(err + '\n');
+          res.end();
+          return;
+        }
+
+        res.writeHead(200);
+        res.write(file, 'binary');
+        res.end();
+      });
+    });
+  }).listen(parseInt(server.port, 10));
+};
+
+
+exports.SimpleHTTPServer = SimpleHTTPServer;
+
+if(process.argv[1]){
+  var server = new SimpleHTTPServer();
+  server.run();
+}
